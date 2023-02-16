cd405e4e445991c5767b271d385cd6887c93fcc2
Ankita Indi <indi.a@northeastern.edu>
2023-02-16 01:06:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "vpc_infra_1" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = var.vpc_enable_dns_hostnames
-  enable_dns_support   = var.vpc_enable_dns_support
+        enable_dns_support   = var.vpc_enable_dns_support
 
   tags = {
     Name        = var.vpc_display_name
